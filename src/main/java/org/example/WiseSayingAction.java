e5900830b2358e7d4f9024b71cd5c70c8f0e256b
qkek123 <qkek622378@gmail.com>
2025-05-31 18:34:03 +0900

    4단계 완료

--- src/main/java/org/example/WiseSayingAction.java
+++ src/main/java/org/example/WiseSayingAction.java
@@ -3,11 +3,14 @@ package org.example;
 import java.util.Scanner;
 
 public class WiseSayingAction {
+    static int count = 0;
+
     public static void register(Scanner sc) {
         System.out.print("명언: ");
         String wiseSaying = sc.nextLine();
         System.out.print("작가: ");
         String author = sc.nextLine();
-        System.out.println("1번 명언이 등록되었습니다.");
+        count++;
+        System.out.printf("%d번 명언이 등록되었습니다.\n", count);
     }
 }
